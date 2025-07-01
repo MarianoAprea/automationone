ce7dfe8d9e7877e5714e68ca3ef4386863af4d6a
Mariano Aprea <apreamariano@gmail.com>
2025-07-01 19:46:24 -0300

    commit 19:46

--- .venv/web/Tests/test_main_web.py
+++ .venv/web/Tests/test_main_web.py
@@ -4,6 +4,10 @@ from web.Setup.main import base_url
 from selenium.webdriver.common.by import By
 import pytest
 
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(options=options)
 
 @pytest.mark.parametrize("driver", [
     (webdriver.Chrome()),
