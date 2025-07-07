639a0576927ac5ab41328c4ba931f54aaccf5548
Mariano Aprea <apreamariano@gmail.com>
2025-07-07 20:57:33 -0300

    modificaciones agrego  optios

--- enviroment/web/Tests/test_mainweb.py
+++ enviroment/web/Tests/test_mainweb.py
@@ -8,7 +8,7 @@ import pytest
 base_url ='https://qa-practice.netlify.app/'
 
 opt = webdriver.ChromeOptions()
-#opt.add_argument("--headless")  #  ejecuta sin mostrar el  navegador
+# opt.add_argument("--headless")  #  ejecuta sin mostrar el  navegador -----falla
 #opt.add_argument("--user-data-dir=/tmp/user-data")
 opt.add_argument("--no-sandbox")
 opt.add_argument("--incognito")
@@ -16,14 +16,13 @@ opt.add_argument("--disable-extensions")
 opt.add_argument('--ignore-certificate-errors')
 #opt.add_argument("--enable-automation")  #  muestra la barra de que es manejado por un automatizador
 
-# @pytest.mark.parametrize("driver", [
-#  (webdriver.Chrome()),
+@pytest.mark.parametrize("driver", [
+    (webdriver.Chrome(options=opt)),
 # (webdriver.Edge()),
 #  (webdriver.Firefox()),
-# ])
-
-def test_login():
-    driver = webdriver.Chrome(options=opt)
+])
+def test_login(driver):
+    #driver= webdriver.Chrome(options=opt)
     driver.get(url=base_url)
     assert "Welcome!" == driver.find_element(by=By.XPATH, value='//*[@id="content"]/div[1]/div/h1').text
     driver.find_element(by=By.XPATH, value='//*[@id="auth-shop"]/b').click()
