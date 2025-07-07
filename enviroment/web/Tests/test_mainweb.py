459019431ae6202b8d9fe3995611b77f29e068f5
Mariano Aprea <apreamariano@gmail.com>
2025-07-07 17:38:16 -0300

    agregado de options a chrome

--- enviroment/web/Tests/test_mainweb.py
+++ enviroment/web/Tests/test_mainweb.py
@@ -1,11 +1,19 @@
 from time import sleep
 from selenium import webdriver
 #from web.setup.principal import base_url
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pytest
 
 base_url ='https://qa-practice.netlify.app/'
 
+opt = webdriver.ChromeOptions()
+#opt.add_argument("--headless")  #  ejecuta sin mostrar el  navegador
+#opt.add_argument("--user-data-dir=/tmp/user-data")
+opt.add_argument("--no-sandbox")
+opt.add_argument("--disable-extensions")
+opt.add_argument('--ignore-certificate-errors')
+#opt.add_argument("--enable-automation")  #  muestra la barra de que es manejado por un automatizador
 
 # @pytest.mark.parametrize("driver", [
 #  (webdriver.Chrome()),
@@ -13,10 +21,8 @@ base_url ='https://qa-practice.netlify.app/'
 #  (webdriver.Firefox()),
 # ])
 
-driver = webdriver.Chrome()
-
-
 def test_login():
+    driver = webdriver.Chrome(options=opt)
     driver.get(url=base_url)
     assert "Welcome!" == driver.find_element(by=By.XPATH, value='//*[@id="content"]/div[1]/div/h1').text
     driver.find_element(by=By.XPATH, value='//*[@id="auth-shop"]/b').click()
