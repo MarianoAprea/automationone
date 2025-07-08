0e901298a9f8090347ccb51a55975f018316377d
Mariano Aprea <apreamariano@gmail.com>
2025-07-07 23:02:24 -0300

    ahora siiiii

--- enviroment/web/Tests/test_mainweb.py
+++ enviroment/web/Tests/test_mainweb.py
@@ -1,14 +1,13 @@
 from time import sleep
 from selenium import webdriver
 #from web.setup.principal import base_url
-# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pytest
 
 base_url ='https://qa-practice.netlify.app/'
 
 opt = webdriver.ChromeOptions()
-opt.add_argument("--headless=new")  #  ejecuta sin mostrar el  navegador -----falla
+# opt.add_argument("--headless=new")  #  ejecuta sin mostrar el  navegador -----falla
 #opt.add_argument("--user-data-dir=/tmp/user-data")
 opt.add_argument("--no-sandbox")
 opt.add_argument("--incognito")
@@ -21,18 +20,23 @@ opt.add_argument('--ignore-certificate-errors')
 # (webdriver.Edge()),
 #  (webdriver.Firefox()),
 # ])
-def test_login():
+def test_dashboard():
     driver = webdriver.Chrome(options=opt)
     driver.get(url=base_url)
     assert "Welcome!" == driver.find_element(by=By.XPATH, value='//*[@id="content"]/div[1]/div/h1').text
-    # driver.find_element(by=By.XPATH, value='//*[@id="auth-shop"]/b').click()
-    # driver.find_element(by=By.XPATH, value='// *[ @ id = "email"]').send_keys('admin@admin.com')
-    # driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys('admin123')
-    # driver.find_element(by=By.ID, value='submitLoginBtn').click()
-    # sleep(1)
-    #assert "SHOPPING CART" == driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/h2').text
     driver.quit()
 
+
+def test_login():
+    driver = webdriver.Chrome(options=opt)
+    driver.get(url=base_url)
+    driver.find_element(by=By.XPATH, value='//*[@id="auth-shop"]/b').click()
+    driver.find_element(by=By.XPATH, value='// *[ @ id = "email"]').send_keys('admin@admin.com')
+    driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys('admin123')
+    driver.find_element(by=By.ID, value='submitLoginBtn').click()
+    sleep(1)
+    assert "SHOPPING CART" == driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/h2').text
+    driver.quit()
 
 def test_exito():
     assert 1 == 1
