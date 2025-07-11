01b4926e194cb52e90abfe1426a2bb3e7bc1a5cd
Mariano Aprea <apreamariano@gmail.com>
2025-07-11 19:21:02 -0300

    nuevo caso para Chuchu!!!

--- enviroment/web/Tests/test_mainweb.py
+++ enviroment/web/Tests/test_mainweb.py
@@ -1,6 +1,7 @@
 from time import sleep
 from selenium import webdriver
-#from web.setup.principal import base_url
+from selenium.webdriver.common.keys import Keys
+from principal import configurardatos
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -35,5 +36,16 @@ def test_login():
     assert "SHOPPING CART" == driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/h2').text
     #driver.close()
 
-def test_exito():
-    assert 1 == 1
+
+def test_compra_dos_prodcutos_iguales():
+    driver.get(url=url)
+    driver.find_element(by=By.ID, value='products-list').click()
+    driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[2]/div/div[5]/div/button').click()
+    # driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/div[2]/div/div[2]/input').click()
+    driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/div[2]/div/div[2]/input').click()
+    for _ in range(1):
+        driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/div[2]/div/div[2]/input').send_keys(
+            Keys.ARROW_UP)
+
+    driver.find_element(by=By.XPATH, value='//*[@id="prooood"]/section[1]/h2').click()
+    assert "$39.98" == driver.find_element(by=By.CLASS_NAME, value='cart-total-price').text
