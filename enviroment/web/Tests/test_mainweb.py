c3317b977c96ef5b74ea262d89e6ff33118425ab
Mariano Aprea <apreamariano@gmail.com>
2025-07-11 13:47:06 -0300

    cambios varios

--- enviroment/web/Tests/test_mainweb.py
+++ enviroment/web/Tests/test_mainweb.py
@@ -1,6 +1,6 @@
 from time import sleep
 from selenium import webdriver
-#from web.setup.principal import base_url
+import principal
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -11,22 +11,24 @@ from selenium.webdriver import Chrome
 #from webdriver_manager.core.os_manager import ChromeType
 import pytest
 
-base_url ='https://qa-practice.netlify.app/'
+url, driver = principal.configurardatos()
 
-opt = Options()
-opt.add_argument('--headless')
-opt.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
-opt.add_argument("--no-sandbox")
-opt.add_argument("--incognito")
-opt.add_argument("--disable-extensions")
-opt.add_argument('--ignore-certificate-errors')
-opt.add_argument('log-level=3')
-opt.add_argument('--disable-dev-shm-usage')
-opt.add_argument('--aggressive-cache-discard')
 
-opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+# base_url ='https://qa-practice.netlify.app/'
 
-service = Service('/usr/local/bin/chromedriver')
+# opt = Options()
+# opt.add_argument('--headless')
+# opt.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+# opt.add_argument("--no-sandbox")
+# opt.add_argument("--incognito")
+# opt.add_argument("--disable-extensions")
+# opt.add_argument('--ignore-certificate-errors')
+# opt.add_argument('log-level=3')
+# opt.add_argument('--disable-dev-shm-usage')
+# opt.add_argument('--aggressive-cache-discard')
+# opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+#service = Service('/usr/local/bin/chromedriver')
 
 
 # @pytest.mark.parametrize("driver", [
@@ -34,16 +36,18 @@ service = Service('/usr/local/bin/chromedriver')
 # (webdriver.Edge()),
 #  (webdriver.Firefox()),
 # ])
-driver = webdriver.Chrome(service=service, options=opt)
+#driver = webdriver.Chrome(service=services, options=opc)
+
+
 def test_dashboard():
-    driver.get(url=base_url)
+    driver.get(url=url)
     assert "Welcome!" == driver.find_element(by=By.XPATH, value='//*[@id="content"]/div[1]/div/h1').text
     driver.quit()
 
 
 def test_login():
     # driver = webdriver.Chrome(service=service, options=opt)
-    driver.get(url=base_url)
+    driver.get(url=url)
     driver.find_element(by=By.XPATH, value='//*[@id="auth-shop"]/b').click()
     driver.find_element(by=By.XPATH, value='// *[ @ id = "email"]').send_keys('admin@admin.com')
     driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys('admin123')
--- enviroment/web/setup/principal.py
+++ enviroment/web/setup/principal.py
@@ -1,10 +1,22 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import tempfile
 
 
+def configurardatos():
+    urldeweb = 'https://qa-practice.netlify.app/'
+    opt = Options()
+    opt.add_argument('--headless')
+    opt.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+    opt.add_argument("--no-sandbox")
+    opt.add_argument("--incognito")
+    opt.add_argument("--disable-extensions")
+    opt.add_argument('--ignore-certificate-errors')
+    opt.add_argument('log-level=3')
+    opt.add_argument('--disable-dev-shm-usage')
+    opt.add_argument('--aggressive-cache-discard')
+    opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+    service = Service('/usr/local/bin/chromedriver')
 
-
-
-
-
+    driver = webdriver.Chrome(service=service, options=opt)
+    return urldeweb, driver
