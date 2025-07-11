2d88a53d3410a1f8173be111c3254954ff78b396
Mariano Aprea <apreamariano@gmail.com>
2025-07-11 12:42:17 -0300

    hoy a ver

--- enviroment/web/Tests/test_mainweb.py
+++ enviroment/web/Tests/test_mainweb.py
@@ -3,13 +3,17 @@ from selenium import webdriver
 #from web.setup.principal import base_url
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import tempfile
 import os
-# import pytest
+from selenium.webdriver import Chrome
+from webdriver_manager.core.os_manager import ChromeType
+import pytest
 
 base_url ='https://qa-practice.netlify.app/'
 
-opt = webdriver.ChromeOptions()
+opt = Options()
 
 opt.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 opt.add_argument("--no-sandbox")
@@ -18,11 +22,13 @@ opt.add_argument("--disable-extensions")
 opt.add_argument('--ignore-certificate-errors')
 opt.add_argument('log-level=3')
 opt.add_argument('--disable-dev-shm-usage')
+opt.add_argument('--aggressive-cache-discard')
 
 opt.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-chromedriver_path = '/home/runner/work/automationone/automationone/enviroment/chromedriver-linux64/chromedriver'
+service = Service('/usr/local/bin/chromedriver')
 
+driver = webdriver.Chrome(service=service, options=options)
 
 # @pytest.mark.parametrize("driver", [
 # (webdriver.Chrome(options=opt)),
@@ -30,7 +36,7 @@ chromedriver_path = '/home/runner/work/automationone/automationone/enviroment/ch
 #  (webdriver.Firefox()),
 # ])
 def test_dashboard():
-    driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=opt)
+    driver = webdriver.Chrome(service=service, options=opt)
     # driver = webdriver.Chrome(options=opt, service=Service(executable_path="/usr/local/bin/chromedriver"))
     driver.get(url=base_url)
     assert "Welcome!" == driver.find_element(by=By.XPATH, value='//*[@id="content"]/div[1]/div/h1').text
@@ -38,7 +44,7 @@ def test_dashboard():
 
 
 def test_login():
-    driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=opt)
+    driver = webdriver.Chrome(service=service, options=opt)
     driver.get(url=base_url)
     driver.find_element(by=By.XPATH, value='//*[@id="auth-shop"]/b').click()
     driver.find_element(by=By.XPATH, value='// *[ @ id = "email"]').send_keys('admin@admin.com')
